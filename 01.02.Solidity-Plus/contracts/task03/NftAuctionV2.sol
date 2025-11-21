6139a89ec7a543bbbeccf779f07643411f653a64
superplanet <superplanet@126.com>
2025-11-21 12:50:24 +0800

    提交修改

--- 01.02.Solidity-Plus/contracts/task03/NftAuctionV2.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-import "./NftAuction.sol";
-
-contract NftAuctionV2 is NftAuction {
-    function testHello() public pure returns (string memory) {
-        return "hello world!";
-    }
-}
