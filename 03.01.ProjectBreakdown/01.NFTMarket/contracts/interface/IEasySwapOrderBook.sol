bdf637072a1589fc012ecc39ce6909829af6456a
superplanet <superplanet@126.com>
2025-12-26 16:48:04 +0800

    提交修改

---
+++ 03.01.ProjectBreakdown/01.NFTMarket/contracts/interface/IEasySwapOrderBook.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {OrderKey, Price, LibOrder} from "../libraries/LibOrder.sol";
+
+interface IEasySwapOrderBook {
+    function makeOrders(LibOrder.Order[] calldata newOrders) external payable returns (OrderKey[] memory newOrderKeys);
+
+    function cancellOrders(OrderKey[] calldata orderKeys) external returns (bool[] memory successes);
+
+    function editOrders(
+        LibOrder.EditDetail[] calldata editDetails
+    ) external payable returns (OrderKey[] memory newOrderKeys);
+
+    function matchOrder(
+        LibOrder.Order calldata sellOrder,
+        LibOrder.Order calldata buyOrder
+    ) external payable;
+
+    function matchOrders(
+        LibOrder.MatchDetail[] calldata matchDetails
+    ) external payable returns (bool[] memory successes);
+}
