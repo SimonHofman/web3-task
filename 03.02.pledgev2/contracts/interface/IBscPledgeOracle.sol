d72a0b35b050473eaa61c77e76eacb70f293cd18
superplanet <superplanet@126.com>
2026-02-13 22:55:15 +0800

    提交修改

--- 03.02.pledgev2/contracts/interface/IBscPledgeOracle.sol
+++ 03.02.pledgev2/contracts/interface/IBscPledgeOracle.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-interface IBscPledgeOracel {
+interface IBscPledgeOracle {
     function getPrice(address asset) external view returns (uint256);
 
     function getUnderlyingPrice(uint256 cToken) external view returns (uint256);
